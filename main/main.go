1cb21e34961453fca08f3ae94cf88f8ff7423910
lneo <neo.lopez@ynov.com>
2023-12-12 11:43:02 +0100

    ok

--- main/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/Stanislasx/hangman"
-
-func main() {
-	// Créez une instance de HangManData pour fournir à la fonction Test
-	vie := hangman.HangManData{}
-	hangman.Test(&vie)
-}
